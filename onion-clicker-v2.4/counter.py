3c1d0df3d6929382b6012f67cb098bb9592a8734
turbosprezarkaa <83269624+turbosprezarkaa@users.noreply.github.com>
2022-07-17 17:57:34 +0200

    Update counter.py

--- onion-clicker-v2.4/counter.py
+++ onion-clicker-v2.4/counter.py
@@ -1,11 +1,17 @@
 def reset():
     global root
-    file1 = open('data.txt', 'r+')
-    file2 = open('num.txt', 'r+')
+    global file2
+    file1 = open('Cache/data.txt', 'r+')
+    file2 = open('Cache/num.txt', 'r+')
+    file3 = open('Cache/inf.txt', 'r+')
     file1.truncate()
     file2.truncate()
+    file3.truncate()
     file2.write('0')
-    messagebox.showinfo('Onion Clicker v2.4','ZRESETOWANO WSZYSTKIE INFORMACJE\n\nWŁĄCZ GRE PONOWNIE')
+    file2.write('6000')
+    file2.flush()
+    file3.flush()
+    messagebox.showinfo('Onion Clicker','ZRESETOWANO WSZYSTKIE INFORMACJE\n\nWŁĄCZ GRE PONOWNIE')
     root.destroy()
 
 def endgame():
@@ -13,7 +19,7 @@ def endgame():
     global liczba
 
     liczba -= 1
-    variablefile = open('num.txt', 'r+')
+    variablefile = open('Cache/num.txt', 'r+')
     variablefile.truncate()
     variablefile.write(str(liczba))
     root.destroy()
@@ -25,24 +31,28 @@ def kasa():
 
     liczba += 1
     licznik.pack_forget()
-    licznik = Label(window, text = f'KASA:{liczba}$', bg = '#ff8400', font = ('Monofonto-Regular', 30, 'italic'))
+    licznik = Label(window, text = f'KASA:{liczba}$', bg = '#ff8400', font = ('jetbrains mono bold', 30))
     licznik.pack(side = TOP)
 
     if liczba >= 10:
         window.destroy()
         root = Tk()
-        root.title('Onion Clicker v2.4')
-        root.geometry('620x500')
+        root.title('Onion Clicker')
+        root.geometry('620x400')
         root.config(bg = '#ff8400')
-        root.resizable(0, 0)
-        root.iconbitmap('cache/onions/icon.ico')
-        label1 = Label(root, text = 'OTO KONIEC GRY!', bg = '#ff8400', font = ('Monofonto-Regular', 35, 'underline'), anchor = CENTER)
+        # root.resizable(0, 0)
+        root.iconbitmap('Cache/Images/icon.ico')
+        images = Image.open('Cache/Images/onion.png')
+        newimgs = ImageTk.PhotoImage(images)
+        onion = Label(root, image = newimgs)
+        onion.pack(side = LEFT)
+        label1 = Label(root, text = 'OTO KONIEC GRY!', bg = '#ff8400', font = ('jetbrains mono bold', 30, 'underline'), anchor = CENTER)
         label1.pack(side = TOP)
-        label2 = Label(root, text = "no chyba, że dodam jakieś\nupdate'y w przyszłości\n¯\_(ツ)_/¯\n\nDZIĘKI ZA ZAGRANIE W\nONION CLICKERA! <3", bg = '#ff8400', font = ('Monofonto-Regular', 25), anchor = CENTER)
+        label2 = Label(root, text = "\nDZIĘKI ZA ZAGRANIE W\nONION CLICKERA! <3", bg = '#ff8400', font = ('jetbrains mono bold', 25), anchor = CENTER)
         label2.pack()
-        button = Button(root, text = '         RESET (opcjonalne)         ', font = ('Monofonto-Regular', 20), bg = '#ff8400', activebackground = '#ff8400', height = 1, relief = SOLID, command = reset)
+        button = Button(root, text = '         RESET (opcjonalne)         ', font = ('jetbrains mono bold', 20), bg = '#ff8400', activebackground = '#ff8400', height = 1, width = 30, relief = SOLID, command = reset)
         button.pack(expand = True)
-        author = Label(root, text = 'made by @turbosprezarkaa', font = ('Monofonto-Regular', 15), bg = '#ff8400')
+        author = Label(root, text = 'made by @turbosprezarkaa', font = ('jetbrains mono bold', 15), bg = '#ff8400')
         author.pack(side = BOTTOM)
         root.protocol('WM_DELETE_WINDOW', endgame)
 
@@ -50,27 +60,28 @@ def kasax2():
     global root
     global liczba
     global licznik
+    global variablefile
 
     liczba += 2
     licznik.pack_forget()
-    licznik = Label(window, text = f'KASA:{liczba}$', bg = '#ff8400', font = ('Monofonto-Regular', 30, 'italic'))
+    licznik = Label(window, text = f'KASA:{liczba}$', bg = '#ff8400', font = ('jetbrains mono bold', 30))
     licznik.pack(side = TOP)
 
     if liczba >= 100000:
         window.destroy()
         root = Tk()
-        root.title('Onion Clicker v2.4')
+        root.title('Onion Clicker')
         root.geometry('620x500')
         root.config(bg = '#ff8400')
         root.resizable(0, 0)
-        root.iconbitmap('cache/onions/icon.ico')
-        label1 = Label(root, text = 'OTO KONIEC GRY!', bg = '#ff8400', font = ('Monofonto-Regular', 35, 'underline'), anchor = CENTER)
+        root.iconbitmap('Cache/Images/icon.ico')
+        label1 = Label(root, text = 'OTO KONIEC GRY!', bg = '#ff8400', font = ('jetbrains mono bold', 35, 'underline'), anchor = CENTER)
         label1.pack(side = TOP)
-        label2 = Label(root, text = "no chyba, że dodam jakieś\nupdate'y w przyszłości\n¯\_(ツ)_/¯\n\nDZIĘKI ZA ZAGRANIE W\nONION CLICKERA! <3", bg = '#ff8400', font = ('Monofonto-Regular', 25), anchor = CENTER)
+        label2 = Label(root, text = "no chyba, że dodam jakieś\nupdate'y w przyszłości\n¯\_(ツ)_/¯\n\nDZIĘKI ZA ZAGRANIE W\nONION CLICKERA! <3", bg = '#ff8400', font = ('jetbrains mono bold', 25), anchor = CENTER)
         label2.pack()
-        button = Button(root, text = '         RESET (opcjonalne)         ', font = ('Monofonto-Regular', 20), bg = '#ff8400', activebackground = '#ff8400', height = 1, relief = SOLID, command = reset)
+        button = Button(root, text = '         RESET (opcjonalne)         ', font = ('jetbrains mono bold', 20), bg = '#ff8400', activebackground = '#ff8400', height = 1, relief = SOLID, command = reset)
         button.pack(expand = True)
-        author = Label(root, text = 'made by @turbosprezarkaa', font = ('Monofonto-Regular', 15), bg = '#ff8400')
+        author = Label(root, text = 'made by @turbosprezarkaa', font = ('jetbrains mono bold', 15), bg = '#ff8400')
         author.pack(side = BOTTOM)
         root.protocol('WM_DELETE_WINDOW', endgame)
 
@@ -78,27 +89,28 @@ def kasax3():
     global root
     global liczba
     global licznik
+    global variablefile
 
     liczba += 3
     licznik.pack_forget()
-    licznik = Label(window, text = f'KASA:{liczba}$', bg = '#ff8400', font = ('Monofonto-Regular', 30, 'italic'))
+    licznik = Label(window, text = f'KASA:{liczba}$', bg = '#ff8400', font = ('jetbrains mono bold', 30))
     licznik.pack(side = TOP)
 
     if liczba >= 100000:
         window.destroy()
         root = Tk()
-        root.title('Onion Clicker v2.4')
+        root.title('Onion Clicker')
         root.geometry('620x500')
         root.config(bg = '#ff8400')
         root.resizable(0, 0)
-        root.iconbitmap('cache/onions/icon.ico')
-        label1 = Label(root, text = 'OTO KONIEC GRY!', bg = '#ff8400', font = ('Monofonto-Regular', 35, 'underline'), anchor = CENTER)
+        root.iconbitmap('Cache/Images/icon.ico')
+        label1 = Label(root, text = 'OTO KONIEC GRY!', bg = '#ff8400', font = ('jetbrains mono bold', 35, 'underline'), anchor = CENTER)
         label1.pack(side = TOP)
-        label2 = Label(root, text = "no chyba, że dodam jakieś\nupdate'y w przyszłości\n¯\_(ツ)_/¯\n\nDZIĘKI ZA ZAGRANIE W\nONION CLICKERA! <3", bg = '#ff8400', font = ('Monofonto-Regular', 25), anchor = CENTER)
+        label2 = Label(root, text = "no chyba, że dodam jakieś\nupdate'y w przyszłości\n¯\_(ツ)_/¯\n\nDZIĘKI ZA ZAGRANIE W\nONION CLICKERA! <3", bg = '#ff8400', font = ('jetbrains mono bold', 25), anchor = CENTER)
         label2.pack()
-        button = Button(root, text = '         RESET (opcjonalne)         ', font = ('Monofonto-Regular', 20), bg = '#ff8400', activebackground = '#ff8400', height = 1, relief = SOLID, command = reset)
+        button = Button(root, text = '         RESET (opcjonalne)         ', font = ('jetbrains mono bold', 20), bg = '#ff8400', activebackground = '#ff8400', height = 1, relief = SOLID, command = reset)
         button.pack(expand = True)
-        author = Label(root, text = 'made by @turbosprezarkaa', font = ('Monofonto-Regular', 15), bg = '#ff8400')
+        author = Label(root, text = 'made by @turbosprezarkaa', font = ('jetbrains mono bold', 15), bg = '#ff8400')
         author.pack(side = BOTTOM)
         root.protocol('WM_DELETE_WINDOW', endgame)
 
@@ -106,26 +118,27 @@ def kasax5():
     global root
     global liczba
     global licznik
+    global variablefile
 
     liczba += 5
     licznik.pack_forget()
-    licznik = Label(window, text = f'KASA:{liczba}$', bg = '#ff8400', font = ('Monofonto-Regular', 30, 'italic'))
+    licznik = Label(window, text = f'KASA:{liczba}$', bg = '#ff8400', font = ('jetbrains mono bold', 30))
     licznik.pack(side = TOP)
 
     if liczba >= 100000:
         window.destroy()
         root = Tk()
-        root.title('Onion Clicker v2.4')
+        root.title('Onion Clicker')
         root.geometry('620x500')
         root.config(bg = '#ff8400')
         root.resizable(0, 0)
-        root.iconbitmap('cache/onions/icon.ico')
-        label1 = Label(root, text = 'OTO KONIEC GRY!', bg = '#ff8400', font = ('Monofonto-Regular', 35, 'underline'), anchor = CENTER)
+        root.iconbitmap('Cache/Images/icon.ico')
+        label1 = Label(root, text = 'OTO KONIEC GRY!', bg = '#ff8400', font = ('jetbrains mono bold', 35, 'underline'), anchor = CENTER)
         label1.pack(side = TOP)
-        label2 = Label(root, text = "no chyba, że dodam jakieś\nupdate'y w przyszłości\n¯\_(ツ)_/¯\n\nDZIĘKI ZA ZAGRANIE W\nONION CLICKERA! <3", bg = '#ff8400', font = ('Monofonto-Regular', 25), anchor = CENTER)
+        label2 = Label(root, text = "no chyba, że dodam jakieś\nupdate'y w przyszłości\n¯\_(ツ)_/¯\n\nDZIĘKI ZA ZAGRANIE W\nONION CLICKERA! <3", bg = '#ff8400', font = ('jetbrains mono bold', 25), anchor = CENTER)
         label2.pack()
-        button = Button(root, text = '         RESET (opcjonalne)         ', font = ('Monofonto-Regular', 20), bg = '#ff8400', activebackground = '#ff8400', height = 1, relief = SOLID, command = reset)
+        button = Button(root, text = '         RESET (opcjonalne)         ', font = ('jetbrains mono bold', 20), bg = '#ff8400', activebackground = '#ff8400', height = 1, relief = SOLID, command = reset)
         button.pack(expand = True)
-        author = Label(root, text = 'made by @turbosprezarkaa', font = ('Monofonto-Regular', 15), bg = '#ff8400')
+        author = Label(root, text = 'made by @turbosprezarkaa', font = ('jetbrains mono bold', 15), bg = '#ff8400')
         author.pack(side = BOTTOM)
         root.protocol('WM_DELETE_WINDOW', endgame)
